9e61ab0bef6ebfffe1ea2adfd5b3d8c7f6c7469d
jstaursky <jstaursky@gmail.com>
2019-10-31 15:56:13 -0400

    Update example-one.cpp

--- examples/example-one/example-one.cpp
+++ examples/example-one/example-one.cpp
@@ -1,11 +1,16 @@
-/*
+
+/* ###
+ * IP: GHIDRA
+ * NOTE: mentions GNU libbfd, the hard-coded binary is a toy function that generates primes
+ *
+ * Modifications copyright (C) 2019 Joe Staursky, no rights reserved (edits are public domain).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
